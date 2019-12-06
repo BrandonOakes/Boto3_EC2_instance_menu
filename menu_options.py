2fcec04080e762d3e7ab6be05bf73c270355a1e4
Brandon Oakes <oakesb@mail.usf.edu>
2019-12-05 22:16:23 -0800

    error handling for menu module

--- menu_options.py
+++ menu_options.py
@@ -2,10 +2,11 @@ import boto3
 from helper_functions import input_n_clear
 
 def main_menu():
-    ''''Main menu options'''
-
-    user_choice = int(input_n_clear('''
-Welcome to Easy EC2!
+    ''''Main menu options for user'''
+    while True:
+        try:
+            user_choice = int(input_n_clear(
+'''Welcome to Easy EC2!
 
 Please enter the number below depending on your desired option:
 
@@ -14,4 +15,9 @@ Please enter the number below depending on your desired option:
 3) Exit
 
 :'''))
-    return user_choice
+            if user_choice in [1,2,3]:
+                return user_choice
+            else:
+                print("Invalid option! Enter 1,2 or 3\n")
+        except Exception as e:
+            print("Invalid input:", e, "\n")
